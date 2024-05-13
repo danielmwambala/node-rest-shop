da38c2635296706d09d9abe05ad5147839abd022
danielmwambala <danielmwambala@gmail.com>
2024-05-14 00:18:04 +0300

    add error handling

--- app.js
+++ app.js
@@ -8,4 +8,21 @@ const ordersRoutes = require("./api/routes/orders");
 app.use("/products", productsRoutes);
 app.use("/orders", ordersRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error(
+    "The requested service was not found on this server."
+  );
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
 module.exports = app;
